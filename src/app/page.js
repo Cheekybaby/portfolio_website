bb68abf221881335d7bdbb5cdb51df73cc4753c7
Shashank Ranjan <shashankranjan07@gmail.com>
2025-01-08 19:03:15 +0530

    get mogged again

--- src/app/page.js
+++ src/app/page.js
@@ -3,10 +3,18 @@ import Body from "@/components/Body";
 import Footer from "@/components/Footer";
 export default function Home() {
   return (
-    <div>
-      <Navbar />
-      <Body />
-      <Footer />
-    </div>
+    <>
+      <head>
+        <title>Shashank Ranjan</title>
+        <link rel="icon" href="/favicon.ico" />
+      </head>
+      <body className="m-0">
+        <Navbar />
+        <main className="flex min-h-screen flex-col container mx-auto px-2 py-2">
+          <Body />
+        </main>
+        <Footer />
+      </body>
+    </>
   );
 }
